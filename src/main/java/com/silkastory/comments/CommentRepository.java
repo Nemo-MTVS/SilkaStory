81373dead86a017ddb2ebe9c08a043a27efea6e1
apfp77 <apfp77@naver.com>
2025-03-28 16:54:57 +0900

    feat: comment 제거

--- src/main/java/com/silkastory/comments/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.silkastory.comments;
-
-import com.silkastory.common.MiniJPARepository;
-
-import java.util.List;
-
-public interface CommentRepository extends MiniJPARepository<Comment, Long> {
-    List<Comment> findByPostId(Long postId);
-}
